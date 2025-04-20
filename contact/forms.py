899152fff45d678e2ea34968ec9d605a9e95de96
Tonilson Dias <tonilsonpedrodias@gmail.com>
2025-04-20 22:30:19 +0100

    Trabalhando com imagemField no template e no forms

--- contact/forms.py
+++ contact/forms.py
@@ -1,57 +1,42 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from . import models 
+ 
+from . import models
+ 
 
 
 class ContactForm(forms.ModelForm):
-
-    first_name = forms.CharField(
-         widget=forms.TextInput(
-             attrs={
-                 'class': 'classe-a classe-b',
-                 'placeholder': 'Aqui veio do init',
-             }
-         ),
-         label='Primeiro Nome',
-         help_text='Texto de ajuda para seu usuário',
-     )
- 
-    def __init__(self, *args, **kwargs):
-         super().__init__(*args, **kwargs)
- 
-         # self.fields['first_name'].widget.attrs.update({
-         #     'class': 'classe-a classe-b',
-         #     'placeholder': 'Aqui veio do init',
-         # })
- 
-    class Meta:
-         model = models.Contact
-         fields = (
+   class Meta:
+        model = models.Contact
+        fields = (
              'first_name', 'last_name', 'phone',
              'email', 'description', 'category',
-         )
-         # widgets = {
-         #     'first_name': forms.TextInput(
-         #         attrs={
-         #             'class': 'classe-a classe-b',
-         #             'placeholder': 'Escreva aqui',
-         #         }
-         #     )
-         # }
- 
-    def clean(self):
+             'picture',
+            )
+
+        picture = forms.ImageField(
+           widget=forms.FileInput(
+             attrs={
+               'accept': 'image/*',
+             }
+          )
+        )
+    
+    
+    
+def clean(self):
         cleaned_data = self.cleaned_data
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
 
         if first_name and last_name and  first_name ==  last_name:
-            erro_msg= "'Primeiro nome nao pode ser igual ao segundo'"
+            erro_msg= 'Primeiro nome nao pode ser igual ao segundo'
             self.add_error('first_name', erro_msg)
             self.add_error('last_name', erro_msg) 
             
         return cleaned_data
     
-    def clean_first_name(self):
+def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
 
         if first_name == 'ABC':
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -14,7 +14,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
         
         context = {
             'form': form,
@@ -53,7 +53,7 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form, 
@@ -72,11 +72,30 @@ def update(request, contact_id):
 
     context = {
         'form': ContactForm(instance=contact),
-        'form_action': form_action,
-       
+         'form_action': form_action,
     }
     return render(
         request,
         'contact/create.html',
         context
+    )
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+    
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )
