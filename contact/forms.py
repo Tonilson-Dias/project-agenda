58dbdb4edba9d6cf8da65676adcb0dbe6dce82a0
Tonilson Dias <tonilsonpedrodias@gmail.com>
2025-04-20 10:37:47 +0100

    verifivando form.is_valid, salvando com form.save e redirecionando a página

--- contact/forms.py
+++ contact/forms.py
@@ -28,6 +28,7 @@ class ContactForm(forms.ModelForm):
          model = models.Contact
          fields = (
              'first_name', 'last_name', 'phone',
+             'email', 'description', 'category',
          )
          # widgets = {
          #     'first_name': forms.TextInput(
@@ -39,22 +40,27 @@ class ContactForm(forms.ModelForm):
          # }
  
     def clean(self):
-        #cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
+        if first_name and last_name and  first_name ==  last_name:
+            erro_msg= "'Primeiro nome nao pode ser igual ao segundo'"
+            self.add_error('first_name', erro_msg)
+            self.add_error('last_name', erro_msg) 
+            
+        return cleaned_data
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+        if first_name == 'ABC':
+             self.add_error(
+                  'first_name',
+                  ValidationError(
+                       'Veio do add_error',
+                       code = 'invalid'
+                  )
+             )
 
-        return super().clean()
+        return first_name
